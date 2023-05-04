12827379ada49aa8b7c264dacbcf02d7ca55ea24
Shiva04122000 <88384578+Shiva04122000@users.noreply.github.com>
2023-05-04 21:20:31 +0530

    reverse the String

--- script.js
+++ script.js
@@ -27,16 +27,6 @@
 
 
 
-// Find the reverse of the string
-
-// var str="shiva";
-// var revStr='';
-// for(var i=str.length-1;i>=0;i--){
-//     revStr=revStr+str.charAt(i)
-// }
-// console.log(revStr);
-
-
 
 //WAP to find missing elements from the array?
 
